84c7a74565e568af77519f01c94c8161aeca8522
JithinKS <jithinks112@gmail.com>
2023-01-25 19:36:04 +0530

    setting value

--- client/leaderboard/scripts/crawlers/crawlNewData/fetchAndAddAliases.cjs
+++ client/leaderboard/scripts/crawlers/crawlNewData/fetchAndAddAliases.cjs
@@ -7,87 +7,36 @@ dotenv.config();
 
 const fetchAndAddAliases = async () => {
   const aliasArray = await fetchAliases();
-  const uniqueAliasArray =  eliminateDuplicates(aliasArray);
-  await addAliases(uniqueAliasArray)
+  console.log(aliasArray);
+  // await addAliases(aliasArray)
 };
 
-const eliminateDuplicates = (aliasArray) => {
-  const addressToIndicesMapping = getAddressToIndicesMapping(aliasArray);
-  const finalResults = []
-  Object.keys(addressToIndicesMapping).forEach((address) => {
-    const indices = addressToIndicesMapping[address];
-    if (indices.length === 1) {
-      finalResults.push(aliasArray[indices[0]]);
-    } else {
-      const entryWithLastCreatedDate = getEntryWithLastCreatedDate(indices.map((index) => aliasArray[index]));
-      finalResults.push(entryWithLastCreatedDate);
-    }
-  });
-  return finalResults
-}
-
-const getEntryWithLastCreatedDate = (aliasArray) => { 
-  let entryWithLastCreatedDate = aliasArray[0];
-  for (let i = 1; i < aliasArray.length; i++) { 
-
-    if (Date.parse(aliasArray[i].createdate) > Date.parse(entryWithLastCreatedDate.createdate)) { 
-      entryWithLastCreatedDate = aliasArray[i];
-    }
-  }
-  return entryWithLastCreatedDate;
-}
-
-const getAddressToIndicesMapping = (aliasArray) => { 
-  const addressToIndices = {};
-  aliasArray.forEach((alias, index) => {
-    if (addressToIndices[alias.ethernaut_address]) { 
-      addressToIndices[alias.ethernaut_address].push(index);
-    } else { 
-      addressToIndices[alias.ethernaut_address] = [index];
-    }
-  });
-  return addressToIndices;
-}
-
 const fetchAliases = async () => {
-  let after = 0;
-  let results;
-  let finalResults = [];
-  do {
-    results = await getFetchAliasRequest(after)
-    after = after + 100
-    finalResults.push(...results)
-  } while (results.length === 100)
-  return finalResults;
-}
-
-const getFetchAliasRequest = async (after) => { 
-  const response = await axios.post('https://api.hubapi.com/crm/v3/objects/contacts/search',
+  const response = await axios.post(
+    "https://api.hubapi.com/crm/v3/objects/companies/search",
     {
       limit: 100,
-      properties: ["email", "ethernaut_address", "ethernaut_alias"],
-      after
     },
     {
       headers: {
-        'Authorization': `Bearer ${process.env.HUBSPOT_SECRET}`,
-        'Content-Type': 'application/json'
-      }
+        Authorization: `Bearer ${process.env.HUBSPOT_SECRET}`,
+        "Content-Type": "application/json",
+      },
     }
-  )
-  return response.data.results.map((result) => result.properties);
-}
+  );
+  return response.data.results.map((entry) => entry.properties);
+};
 
 const addAliases = async (aliasArray) => {
   const leaderBoard = JSON.parse(fs.readFileSync(leaderBoardPath));
 
   const newLeaderBoard = leaderBoard.map((entry) => {
-    for(let i = 0; i < aliasArray.length; i++) {
-      if(entry.player === aliasArray[i].player) {
+    for (let i = 0; i < aliasArray.length; i++) {
+      if (entry.player === aliasArray[i].player) {
         return {
           ...entry,
-          alias: aliasArray[i].alias
-        }
+          alias: aliasArray[i].alias,
+        };
       }
     }
     return entry;
@@ -96,6 +45,6 @@ const addAliases = async (aliasArray) => {
   fs.writeFileSync(leaderBoardPath, JSON.stringify(newLeaderBoard));
 };
 
-fetchAndAddAliases()
+fetchAndAddAliases();
 
 module.exports = fetchAndAddAliases;
